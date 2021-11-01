aa467577dfa66b478c26ffb952d580938570a29d
SAIKAT12000448 <73012048+SAIKAT12000448@users.noreply.github.com>
2021-11-01 21:30:35 +0600

    complete

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ async function run() {
     await client.connect();
     const database = client.db("PicnicSpot");
     const planCollection = database.collection("picnic");
+    const anotherdata = client.db('myspot');
+    const myCollection = anotherdata.collection("mybook")
    
     // post
     app.post('/addplans',async(req,res)=>{
@@ -33,12 +35,63 @@ async function run() {
            res.send(result)
     })
 
+
+    app.post('/bookinfo',async(req,res)=>{
+          console.log(req.body)
+          const book=req.body;
+           console.log(book)
+          const result = await myCollection.insertOne(book)
+         
+           res.send(result)
+    })
+
+
+    app.get('/bookinfo',async(req,res)=>{
+      let query = {}
+      const displayName = req.query.name;
+      if(displayName){
+        query ={displayName :displayName}
+
+      }
+            const cursor = myCollection.find(query);
+            const books = await cursor.toArray(); 
+            res.json(books)
+    })
+
+    app.get('/allbooking',async(req,res)=>{
+    
+            const cursor = myCollection.find({});
+            const books = await cursor.toArray(); 
+            res.json(books)
+    })
+
+
+   
+
     // get
 
     app.get('/plans',async(req,res)=>{
       const cursor = planCollection.find({})
      const getplan  = await cursor.toArray();
      res.send(getplan)
+
+
+// manageall
+app.get("/manageall", async (req, res) => {
+  const result = await planCollection.find({}).toArray();
+  res.send(result);
+});
+
+
+
+
+
+     app.get("/myplans/:displayName", async (req, res) => {
+      const result = await EventsCollection.find({
+        displayName: req.params.displayName,
+      }).toArray();
+      res.send(result);
+    });
      
     })
 
@@ -53,7 +106,14 @@ async function run() {
     })
 
 
-    // get my orders
+  //  delete
+  app.delete("/deletebook/:id", async (req, res) => {
+    console.log(req.params.id);
+    const result = await myCollection.deleteOne({
+      _id: ObjectId(req.params.id),
+    });
+    res.send(result);
+  });
     
 
 
